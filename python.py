c1aff7133a81ef0089004ea5298451ee4953fb2d
rk2-182 <ricardoplus@live.cl>
2021-09-15 16:37:25 -0300

    primer guardado

---
+++ python.py
@@ -0,0 +1,6 @@
+print(" ***** Hola Mundo Prueba Git *****")
+
+print("1.-Paso uno es 'git init' para inicializar")
+print("2.-Paso dos es 'git add' para mover a la are de ensayo")
+print("3.-Paso tres es 'git commit -m' mover a repositorio local<--'instantanea'")
+print("4.-")
