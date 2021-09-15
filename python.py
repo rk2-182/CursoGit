15d6c48e30135aee614cfb5b25e75e28f7105531
rk2-182 <ricardoplus@live.cl>
2021-09-15 16:49:32 -0300

    segundo guardado

--- python.py
+++ python.py
@@ -3,4 +3,5 @@ print(" ***** Hola Mundo Prueba Git *****")
 print("1.-Paso uno es 'git init' para inicializar")
 print("2.-Paso dos es 'git add' para mover a la are de ensayo")
 print("3.-Paso tres es 'git commit -m' mover a repositorio local<--'instantanea'")
-print("4.-")
+#git config --global user.usuername "nombre"
+print("4.-En este paso puede que pida usuario ")
