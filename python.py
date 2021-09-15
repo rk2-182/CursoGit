f99eb5d59c69ce208dc2637c1af3e94a73f22f9c
rk2-182 <ricardoplus@live.cl>
2021-09-15 16:53:57 -0300

    tercer guardado

--- python.py
+++ python.py
@@ -5,3 +5,4 @@ print("2.-Paso dos es 'git add' para mover a la are de ensayo")
 print("3.-Paso tres es 'git commit -m' mover a repositorio local<--'instantanea'")
 #git config --global user.usuername "nombre"
 print("4.-En este paso puede que pida usuario ")
+print("5.-git log --oneline Ver copias que existen en el repositorio local")
