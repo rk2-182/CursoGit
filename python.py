d1372940e3b6f1330bf823bd9e564cd5ed014eef
rk2-182 <ricardoplus@live.cl>
2021-09-20 09:10:25 -0300

    ultimo commit

--- python.py
+++ python.py
@@ -7,5 +7,5 @@ print("3.-Paso tres es 'git commit -m' mover a repositorio local<--'instantanea'
 print("4.-En este paso puede que pida usuario ")
 print("5.-git log --oneline Ver copias que existen en el repositorio local")
 print("6.-git reset --hard 'numeroInstantanea' Retroceder a instantanea seleccionada ")
-print("7.-Suibir proyecto: git remote add origin 'direccion github'")
-
+print("7.-Agregar repositorio proyecto: git remote add origin 'direccion github'")
+print("8.-Subir archivos al repositorio: git push -u origin main")
